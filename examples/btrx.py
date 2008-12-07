de0e861ac6dedb458781bd3089873d69178971b8
Mike Ossmann <mike@ossmann.com>
2008-12-07 04:43:24 +0000

    exceptions and cleanup

--- examples/btrx.py
+++ examples/btrx.py
@@ -13,7 +13,6 @@ from gnuradio import usrp
 from gnuradio import bluetooth
 from gnuradio.eng_option import eng_option
 from optparse import OptionParser
-import sys
 
 class my_top_block(gr.top_block):
 
@@ -38,10 +37,10 @@ class my_top_block(gr.top_block):
 						help="use named input file instead of USRP")
 		parser.add_option("-l", "--lap", type="string", default=None,
 						help="LAP of the master device")
-		parser.add_option("-r", "--sample-rate", type="eng_float", default=0,
-						help="sample rate of input (default: use -d)")
+		parser.add_option("-r", "--sample-rate", type="eng_float", default=None,
+						help="sample rate of input (default: use DECIM)")
 		parser.add_option("-s", "--input-shorts", action="store_true", default=False,
-						help="input interleaved shorts in stead of complex floats")
+						help="input interleaved shorts instead of complex floats")
 		parser.add_option("-p", "--packets", type="int", default=100,
 						help="Number of packets to sniff (default=100)")
 		parser.add_option("-u", "--uap", type="string", default=None,
@@ -59,9 +58,10 @@ class my_top_block(gr.top_block):
 
 		# the demodulator needs at least two samples per symbol
 		min_samples_per_symbol = 2
+		min_sample_rate = symbol_rate * min_samples_per_symbol
 
 		# use options.sample_rate unless not provided by user
-		if options.sample_rate == 0:
+		if options.sample_rate is None:
 			if options.usrp2:
 				# original source is USRP2
 				adc_rate = 100e6
@@ -69,6 +69,10 @@ class my_top_block(gr.top_block):
 				# assume original source is USRP
 				adc_rate = 64e6
 			options.sample_rate = adc_rate / options.decim
+
+		# make sure we have a high enough sample rate
+		if options.sample_rate < min_sample_rate:
+			raise ValueError, "Sample rate (%d) below minimum (%d)\n" % (options.sample_rate, min_sample_rate)
 
 		if options.input_shorts:
 			input_size = gr.sizeof_short
@@ -84,10 +88,10 @@ class my_top_block(gr.top_block):
 			else:
 				src = usrp.source_c(decim_rate=options.decim)
 				subdev = usrp.selected_subdev(src, options.rx_subdev_spec)
+				print "Using RX board %s" % (subdev.side_and_name())
 				r = src.tune(0, subdev, options.freq)
 				if not r:
-					sys.stderr.write('Failed to set USRP frequency\n')
-					raise SystemExit, 1
+					raise SystemExit, "Failed to set USRP frequency"
 				if options.gain is None:
 					# if no gain was specified, use the mid-point in dB
 					g = subdev.gain_range()
@@ -100,18 +104,27 @@ class my_top_block(gr.top_block):
 			# input from file
 			src = gr.file_source(input_size, options.input_file)
 
+		# stage 1: limit input to desired number of samples
+		if options.nsamples is None:
+			stage1 = src
+		else:
+			head = gr.head(input_size, int(options.nsamples))
+			self.connect(src, head)
+			stage1 = head
+	
+		# stage 2: convert input from shorts if necessary
 		if options.input_shorts:
 			s2c = gr.interleaved_short_to_complex()
-			self.connect(src, s2c)
-			complex_src = s2c
+			self.connect(stage1, s2c)
+			stage2 = s2c
 		else:
-			complex_src = src
-	
+			stage2 = stage1
+
 		# coefficients for filter to select single channel
 		channel_filter = gr.firdes.low_pass(1.0, options.sample_rate, 500e3, 500e3, gr.firdes.WIN_HANN)
 
 		# we will decimate by the largest integer that results in enough samples per symbol
-		decimation_rate = int(options.sample_rate/(symbol_rate*min_samples_per_symbol))
+		decimation_rate = int(options.sample_rate/(min_sample_rate))
 		samples_per_symbol = (options.sample_rate/decimation_rate)/symbol_rate
 
 		# look for packets on each channel specified by options.ddc
@@ -141,11 +154,7 @@ class my_top_block(gr.top_block):
 				dst = bluetooth.UAP(int(options.lap, 16), options.packets)
 		
 			# connect the blocks
-			if options.nsamples is None:
-					self.connect(complex_src, ddc, demod, dst)
-			else:
-				head = gr.head(gr.sizeof_gr_complex, int(options.nsamples))
-				self.connect(complex_src, head, ddc, demod, dst)
+			self.connect(stage2, ddc, demod, dst)
 
 if __name__ == '__main__':
 	try:
